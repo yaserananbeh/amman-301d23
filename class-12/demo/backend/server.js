55320ade8aa62803739306588de78a5ea4a4fa79
thamudi <tamim.hamoudi@gmail.com>
2021-06-21 10:14:05 +0300

    Class 12 Demo - starter code added

---
+++ class-12/demo/backend/server.js
@@ -0,0 +1,30 @@
+ 
+// frameworks are code that executes or runs your own code
+// libraries are code that gets executed by your own code
+
+const express = require('express') // require the express package
+const app = express() // initialize your express app instance
+// const  = require('./controller/.controller');
+const indexController = require('./controller/index.controller');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const cors = require('cors'); // enable the communication between the frontend and the backend
+
+app.use(cors())
+
+/*
+-------------------------------
+Our routes
+-------------------------------
+*/
+
+// Our Index Route
+app.get('/', indexController);
+// Our  Route
+// app.get('/', );
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
+// kick start the express server to work
